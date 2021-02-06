34a948189d3c4a940197373107b6886053f9aefa
Blaz Kristan <blaz@kristan-sp.si>
2021-02-06 19:00:18 +0100

    More Tetris optimisations.

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -3138,11 +3138,11 @@ uint16_t WS2812FX::mode_tetris(void) {
   }
   
   if (SEGENV.step == 0) {             //init
-    drop->speed = 0.0238 * (SEGMENT.speed ? (SEGMENT.speed>>4)+1 : random8(5,20)); // set speed
+    drop->speed = 0.0238 * (SEGMENT.speed ? (SEGMENT.speed>>4)+1 : random8(3,20)); // set speed
     drop->pos   = SEGLEN-1;           // start at end of segment
     drop->col   = color_from_palette(random8(0,15)<<4,false,false,0);     // limit color choices so there is enough HUE gap
     SEGENV.step = 1;                  // drop state (0 init, 1 forming, 2 falling)
-    SEGENV.aux0 = random8(1,4+(SEGMENT.intensity>>5)) * (1+(SEGLEN>>6));  // size of brick
+    SEGENV.aux0 = (SEGMENT.intensity ? (SEGMENT.intensity>>5)+1 : random8(1,5)) * (1+(SEGLEN>>6));  // size of brick
   }
   
   if (SEGENV.step == 1) {             // forming
