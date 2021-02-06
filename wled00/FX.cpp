5b8541e1373e8359df20c9fcf5c46e3159df9fbd
Blaz Kristan <blaz@kristan-sp.si>
2021-02-06 18:32:21 +0100

    Mor speed fixes.

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -3138,7 +3138,7 @@ uint16_t WS2812FX::mode_tetris(void) {
   }
   
   if (SEGENV.step == 0) {             //init
-    drop->speed = 0.0238 * (SEGMENT.speed ? (SEGMENT.speed>>4)+1 : random8(1,10)); // set speed
+    drop->speed = 0.0238 * (SEGMENT.speed ? (SEGMENT.speed>>4)+1 : random8(5,20)); // set speed
     drop->pos   = SEGLEN-1;           // start at end of segment
     drop->col   = color_from_palette(random8(0,15)<<4,false,false,0);     // limit color choices so there is enough HUE gap
     SEGENV.step = 1;                  // drop state (0 init, 1 forming, 2 falling)
